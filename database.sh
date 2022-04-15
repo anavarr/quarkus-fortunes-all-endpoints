17cf48fc6165582d93c197526082d886ca3ec8fc
Clement Escoffier <clement@apache.org>
2022-04-15 11:10:10 +0200

    THE DEMO!

---
+++ database.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run --rm=true --name quarkus_test \
+  -e POSTGRES_USER=quarkus_test \
+  -e POSTGRES_PASSWORD=quarkus_test \
+  -e POSTGRES_DB=quarkus_test \
+  -p 5432:5432 postgres:14.2
